859db9c548473e41d4bac4ae83ee0d68118e6c10
Tomas Korcak <korczis@gmail.com>
2014-08-17 16:20:13 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-esri
-====
+# esri
 
 Parser and Processor of http://www.baruch.cuny.edu/geoportal/data/esri/esri_usa.htm
---
+++ run.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+# Fetching links
+bundle exec ruby ./main.rb | tee links.txt
+
+aria2c -i links.txt -x 16 --dir=data
