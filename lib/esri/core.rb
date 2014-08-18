74f1fdc7ad1537a283f1473b45cb828e30058ed2
Tomas Korcak <korczis@gmail.com>
2014-08-17 23:49:51 -0700

    PostGIS Helper Cosmetics

--- lib/esri/core.rb
+++ lib/esri/core.rb
@@ -9,5 +9,7 @@ module Esri
   LIB_DIR = File.join(BASE_DIR, 'lib')
   SRC_DIR = File.join(LIB_DIR, 'esri')
   TMP_DIR = File.join(BASE_DIR, 'tmp')
+
   LINK_FILE = File.join(TMP_DIR, 'links.txt')
+  DB_CONFIG_FILE = File.join(DATA_DIR, 'db.json')
 end
--- lib/esri/helpers/postgis_helper.rb
+++ lib/esri/helpers/postgis_helper.rb
@@ -1,19 +1,32 @@
 # encoding: UTF-8
 
+require 'multi_json'
+
+require_relative '../core'
+
 module Esri
   module Helpers
     # PostGIS helpers
     module PostgisHelper
       class << self
-        def import_shapes(shapes)
+        def load_db_config
+          {
+            host: 'apollocrawler.com',
+            username: 'datathon',
+            password: 'datathon',
+            database: 'datathon_v2'
+          }
+        end
+
+        def import_shapes(shapes, opts = load_db_config)
           shapes.map do |shape|
             table_name = shape.split('/').last.gsub(/\.shp$/, '')
             puts "Importing shape file '#{shape}' as '#{table_name}'"
 
             shp2psql_cmd = "shp2pgsql -c #{shape} public.gis_esri_#{table_name}"
-            psql_cmd = 'psql -h apollocrawler.com -v -d datathon -U datathon'
+            psql_cmd = "psql -v -h #{opts[:host]} -d datathon -U datathon"
 
-            pswd = 'PGPASSWORD=datathon'
+            pswd = 'export  PGPASSWORD=datathon'
             cmd = "#{pswd} && #{shp2psql_cmd} | #{psql_cmd} > /dev/null"
             system cmd
           end
