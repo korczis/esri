8af93dcd88a4a8e10660c232fd379115febf8d1a
Tomas Korcak <korczis@gmail.com>
2014-08-18 01:01:55 -0700

    Fix cop issues

--- lib/esri/helpers/postgis_helper.rb
+++ lib/esri/helpers/postgis_helper.rb
@@ -18,13 +18,13 @@ module Esri
           }
         end
 
-        def import_shapes(shapes, opts = load_db_config)
+        def import_shapes(shapes, _opts = load_db_config)
           shapes.map do |shape|
             table_name = shape.split('/').last.gsub(/\.shp$/, '')
             puts "Importing shape file '#{shape}' as '#{table_name}'"
 
             shp2psql_cmd = "shp2pgsql -c #{shape} public.gis_esri_#{table_name}"
-            psql_cmd = "psql -h apollocrawler.com -d datathon -U datathon"
+            psql_cmd = 'psql -h apollocrawler.com -d datathon -U datathon'
 
             pswd = 'export  PGPASSWORD=datathon'
             cmd = "#{pswd} && #{shp2psql_cmd} | #{psql_cmd} > /dev/null"
